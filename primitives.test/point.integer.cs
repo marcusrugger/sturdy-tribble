c498665c0f818d6abaf22331abdd1d715ec7a824
Marcus Rugger <marcusrugger@yahoo.com>
2019-08-27 21:37:17 -0400

    Initial commit of unit tests for primitives

---
+++ primitives.test/point.integer.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SturdyTribble.Primitive
+
+namespace primitives.test
+{
+    [TestClass]
+    public class TestPointInteger
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
