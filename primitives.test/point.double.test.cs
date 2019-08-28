6d9fdcd09169d798ad6ee3e5f182cc7c2f4c4cfc
Marcus Rugger <marcusrugger@yahoo.com>
2019-08-28 07:33:50 -0400

    Add unit tests for PointDouble

---
+++ primitives.test/point.double.test.cs
@@ -0,0 +1,48 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SturdyTribble.Primitive;
+
+namespace SturdyTribble.Primitive.Tests
+{
+    [TestClass]
+    public class TestPointDouble
+    {
+        [TestMethod]
+        public void ToString_is_formatted_correctly()
+        {
+            var point = new PointDouble(3.14, 5.7);
+            var actual = point.ToString();
+            var expected = "(3.14, 5.7)";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Add_two_points()
+        {
+            double ax = 3.14;
+            double ay = 5.7;
+            double bx = 7.13;
+            double by = 11.17;
+
+            var point1 = new PointDouble(ax, ay);
+            var point2 = new PointDouble(bx, by);
+            var actual = point1 + point2;
+            Assert.AreEqual(ax + bx, actual.X);
+            Assert.AreEqual(ay + by, actual.Y);
+        }
+
+        [TestMethod]
+        public void Subtract_two_points()
+        {
+            double ax = 3.14;
+            double ay = 5.7;
+            double bx = 7.13;
+            double by = 11.17;
+
+            var point1 = new PointDouble(ax, ay);
+            var point2 = new PointDouble(bx, by);
+            var actual = point1 - point2;
+            Assert.AreEqual(ax - bx, actual.X);
+            Assert.AreEqual(ay - by, actual.Y);
+        }
+    }
+}
