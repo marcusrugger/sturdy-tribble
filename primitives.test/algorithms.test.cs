a849cbeb3edc629e7d1fa3e55f36e1dbe9e1824f
Marcus Rugger <marcusrugger@yahoo.com>
2019-08-28 08:53:38 -0400

    Add more Algorithm tests

--- primitives.test/algorithms.test.cs
+++ primitives.test/algorithms.test.cs
@@ -13,7 +13,7 @@ namespace SturdyTribble.Primitive.Tests
             TestAngle(-180, -Math.PI);
             TestAngle(-90, -Math.PI/2);
             TestAngle(0.0, 0.0);
-            TestAngle( 1.234567, 0.021547);
+            TestAngle( 1.234567, 0.02154726, 0.00000001);
             TestAngle(60.000000, Math.PI/3);
             TestAngle(90, Math.PI/2);
             TestAngle(180, Math.PI);
@@ -22,6 +22,37 @@ namespace SturdyTribble.Primitive.Tests
             {
                 var radians = Algorithms.ToRadians(degrees);
                 Assert.AreEqual(expected, radians, precision);
+            }
+        }
+
+        [TestMethod]
+        public void ToDegrees()
+        {
+            TestAngle(-Math.PI, -180);
+            TestAngle(0.0, 0.0);
+            TestAngle(0.02154726, 1.234567);
+            TestAngle(Math.PI/3, 60);
+            TestAngle(Math.PI/2, 90);
+            TestAngle(Math.PI, 180);
+
+            void TestAngle(double radians, double expected, double precision = 0.000001)
+            {
+                var degrees = Algorithms.ToDegrees(radians);
+                Assert.AreEqual(expected, degrees, precision);
+            }
+        }
+
+        [TestMethod]
+        public void Pythagoras()
+        {
+            TestPythagoras(3, 4, 5);
+            TestPythagoras(5, 12, 13);
+            TestPythagoras(9, 12, 15);
+
+            void TestPythagoras(double x, double y, double expected, double precision = 0.000001)
+            {
+                var result = Algorithms.PythagoreanTheorem(x, y);
+                Assert.AreEqual(expected, result, precision);
             }
         }
     }
